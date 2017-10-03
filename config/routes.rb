c30604ffac90b87ba4475a40df7f90f0f4e57534
nitinsampath <nitinsampath@Nitins-MacBook-Pro.local>
2017-10-03 15:12:46 -0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/teachers/new', to: 'teachers#new'
   post '/teachers', to: 'teachers#create'
-  root to: 'students#new'
+  #root to: 'students#new'
+  get '/', to: redirect('/students/new')
   get '/students/new', to: 'students#new'
   post '/students', to: 'students#create'
 end
